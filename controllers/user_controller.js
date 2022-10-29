a800cf847be1384e26b434b7bf121a84f34a09c2
KAHNU CHARAN SAHOO <kanhus082@gmail.com>
2022-10-29 09:24:12 +0530

    creat new one

---
+++ controllers/user_controller.js
@@ -0,0 +1,98 @@
+// const { model } = require("mongoose");
+
+// model.exports.profile = function(req, res){
+//     res.end('<h1>user profile</h1>');
+// }
+const User = require('../models/user');
+
+
+module.exports.profile = function(req, res){
+    // res.end('<h1>User profile</h1>')
+   console.log('line 11',req.user);
+    User.findById(req.user._id, function(err,user){
+        res.render('user_profile',{
+            title : 'User profile',
+            user:user
+        });
+    })
+   
+}
+//render the sign up page
+module.exports.signUp = function(req, res){
+
+if (req.isAuthenticated ()){
+   return res.redirect('/user/profile');
+}
+
+    return res.render('user_sign_up',{
+        title: "Codeial | sign Up"
+    })
+}
+
+//render the sign in page
+module.exports.signIn = function(req,res){
+
+    if (req.isAuthenticated ()){
+        return res.redirect('/user/profile');
+    }
+
+    return res.render('user_sign_in',{
+        title: "Codeial | sign In"
+    })
+}
+//get the sign up data
+module.exports.create = function (req, res) {
+    console.log(req.body)
+   if(req.body.password != req.body.confirm_password){
+    return res.redirect('back');
+   }
+   User.findOne({email: req.body.email},function(err,user){
+    if(err){console.log('error in finding user in signin up'); return}
+    if(!user){
+        User.create(req.body,function(err, user){
+            if(err){console.log('error in creating user while signing up');return}
+
+            return res.redirect('/user/sign-in')
+        })
+    }else{
+        return res.redirect('back');
+    }
+   });
+}
+
+//sing in and create a session for the user
+module.exports.createSession = function(req, res){
+    //steps to authenticate
+    //find the user
+//   User.findOne({email: req.body.email}, function(err, user) {
+//     if(err){console.log('error in creating user while signing in');return}
+//    //handel user found
+//   if (user){
+//  ///handel password which doesn't match
+//    if(user.password != req.body.password){
+//     return res.redirect('back');
+//    }
+//      ///handle session creation
+//       res.cookie('user_id',user.id);
+//       return res.redirect('/user/profile');
+return res.redirect('/');
+// return res.redirect('/user/profile');
+//   }else{
+//   //handel user not found 
+//   return res.redirect('back');
+//   }
+//   });
+     
+
+}
+
+
+module.exports.destroySession = function(req, res){
+    req.logout(function(err){
+        if(err){
+             console.log(err);
+         }
+         console.log('success',"You have Logged out successfully");
+         return res.redirect('/');
+     });
+}
