219a46f25ac9e42ddc6a599b2637214d62aa283a
KAHNU CHARAN SAHOO <kanhus082@gmail.com>
2022-11-03 11:31:32 +0530

    update

---
+++ controllers/posts_controller.js
@@ -0,0 +1,17 @@
+const Post = require('../models/post')
+
+module.exports.create = function(req, res){
+    console.log(req.body.content);
+   
+    Post.create({
+        
+       content: req.body.content,
+       user: req.user._id 
+    }, 
+    function(err, post){
+     if(err){console.log('error in creating a post'); return;}
+     console.log(post);
+     return res.redirect('back');
+    
+    });
+}
--- routs/index.js
+++ routs/index.js
@@ -3,9 +3,11 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
+
 console.log('router loaded');
 
 router.get('/', homeController.home);
 router.use('/user', require('./user')); 
+router.use('/posts', require('./posts'));
 
 module.exports = router;
