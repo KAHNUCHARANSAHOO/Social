cfeabc5b943ee0244a8022e3ece9522fe13c69d1
KAHNU CHARAN  SAHOO <114457826+KAHNUCHARANSAHOO@users.noreply.github.com>
2022-11-08 06:28:22 +0530

    kahnu

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -6,10 +6,19 @@ module.exports.create = async function(req, res){
 
  try {
     console.log(req.body.content);
-    await Post.create({ 
+  let post = await Post.create({ 
        content: req.body.content,
        user: req.user._id 
     });
+    if(req.xhr){
+        return res.status(200).json({
+            data:{
+                post: post
+            },
+            message: "Post created!"
+        });
+    }
+
     req.flash('success', 'Post published');
     return res.redirect('back');
  } catch (err) {
