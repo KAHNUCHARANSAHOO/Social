e68b9fdfe56699651aba4efe87e5c24a698be01f
KAHNU CHARAN  SAHOO <114457826+KAHNUCHARANSAHOO@users.noreply.github.com>
2022-11-09 15:58:43 +0530

    add some portion

--- routs/user.js
+++ routs/user.js
@@ -5,20 +5,20 @@ const passport = require('passport');
  const usersController = require('../controllers/user_controller');
  
  router.get('/profile/:id',passport.checkAuthentication , usersController.profile);
- router.post('/update/:id',passport.checkAuthentication , usersController.update);
+
 
 
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
-
+router.get('/sign-out', usersController.destroySession);
 router.post('/create', usersController.create);
-
+router.post('/update/:id',passport.checkAuthentication , usersController.update);
 //use passport as a middleware to authenticate
 router.post('/create-session', passport.authenticate(
     'local',
     {failureRedirect: '/user/sign-in'},
 ) , usersController.createSession);
 
-router.get('/sign-out', usersController.destroySession);
+
 
 module.exports = router; 
