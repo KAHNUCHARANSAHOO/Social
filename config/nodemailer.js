79ecb95d55e364638567f1138f9bd40363e5d96b
KAHNU CHARAN  SAHOO <114457826+KAHNUCHARANSAHOO@users.noreply.github.com>
2023-01-17 19:25:54 +0530

    17-1-2023

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,5 +1,38 @@
 const nodemailer = require("nodemailer");
+const ejs = require('ejs');
+const path = require('path')
 
 
 
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth:{
+        user:'youthmotivation376@gmail.com',
+        pass: 'Kanhu@123'
+    }
+});
 
+
+let renderTemplate = (data, relativePath) => {
+   let mailHTML;
+   ejs.renderFile(
+    path.join(__dirname, '../views/mailers', relativePath),
+    data,
+    function(err, template){
+        if (err){console.log('error in rendering template'); return}
+
+        mailHTML = template;
+
+    }
+   )
+
+   return mailHTML;
+}
+
+module.exports = {
+    transporter: transporter,
+    renderTemplate: renderTemplate
+}
